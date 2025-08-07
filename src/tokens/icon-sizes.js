0c495720852e8f54337fc13b89aacf962a78af46
mariabpulgar <mariabpulgar@gmail.com>
2025-08-07 13:46:59 -0400

    Actualizar IconSelector y agregar icon-sizes

---
+++ src/tokens/icon-sizes.js
@@ -0,0 +1,7 @@
+export const iconSizeClasses = {
+  small: 'icon-size-small',
+  medium: 'icon-size-medium',
+  large: 'icon-size-large',
+  extraLarge: 'icon-size-extraLarge',
+  display: 'icon-size-display'
+};
