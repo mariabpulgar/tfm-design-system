556e7c6639f1b8f8523f4b12d80b5d5dc09767d4
mariabpulgar <mariabpulgar@gmail.com>
2025-08-14 18:20:45 -0400

    estilos de los botones

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css'
-import StatusLabel from './components/StatusLabel'
+import Divider from './components/Divider'
+import Button from './components/Button'
 
 
 function App() {
   return (
-    <div className="centrando-el-div">
-      <StatusLabel
-        text="Status default"
-        variant="warningIcon"
+    <div>
+      <p>Hello world</p>
+      <Divider variant="fullWidth" />
+      <p>Bye world</p>
+      <Button
+        onClick={() => {}}
+        size="medium"
+        text="Error Button"
+        variant="btn-error"
       />
     </div>    
   );
