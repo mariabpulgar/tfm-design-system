3effb4d787f0b2fa44e37d039bf3c932a9599c81
mariabpulgar <mariabpulgar@gmail.com>
2025-08-16 12:35:29 -0400

    cualquier cambio en la rama antes de cambiar de pc

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css'
 import AlertBox from './components/AlertBox';
+import ButtonGroup from './components/ButtonGroup'
 
 
 function App() {
   return (
     <div>
+      <ButtonGroup></ButtonGroup>
       <AlertBox></AlertBox>
     </div>    
   );
