029ea0e5b718ed5bb10a711edaf32a449ab5f596
mariabpulgar <mariabpulgar@gmail.com>
2025-08-31 12:24:34 -0400

    NavBar width adjustment

--- src/App.jsx
+++ src/App.jsx
@@ -16,14 +16,7 @@ function App() {
         imageSrc="/src/assets/Rectangle982.svg"
         vectorSrc="data:image/svg+xml,%3csvg%20width='2'%20height='38'%20viewBox='0%200%202%2038'%20fill='none'%20xmlns='http://www.w3.org/2000/svg'%3e%3cpath%20d='M1%200V38'%20stroke='%23C2E1F0'/%3e%3c/svg%3e"
       />
-      {/* <Hero 
-        imageSrc={Ellipse5}
-        smallTitle="Lorem ipsum"
-        mainTitle="dolor sit amet, consectetur
-adipiscing elit "
-        buttonText="Button"
-        onButtonClick={() => console.log('Usuario interesado!')}
-      /> */}
+
       <div className="hero">
           <Hero
             smallTitle="Lorem ipsum"
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -45,109 +45,111 @@ function NavBar({
   };
 
   return (
-    <div className="nav-bar" role="navigation" aria-label="Main">
-      <div className="navbar-img">
-        <img src={imageSrc} alt="Logo" fetchPriority="high" />
-      </div>
-
-      <div className="navbar-items">
-        <a href="#">Quiénes somos</a>
-
-        <div className="vector-divider">
-          <img src={vectorSrc} alt="" aria-hidden="true" role="presentation" />
+    <div className="nav-bar-container">
+      <div className="nav-bar" role="navigation" aria-label="Main">
+        <div className="navbar-img">
+          <img src={imageSrc} alt="Logo" fetchPriority="high" />
         </div>
 
-        <a href="#">Ubicación</a>
+        <div className="navbar-items">
+          <a href="#">Quiénes somos</a>
 
-        <div className="vector-divider">
-          <img src={vectorSrc} alt="" aria-hidden="true" role="presentation" />
-        </div>
+          <div className="vector-divider">
+            <img src={vectorSrc} alt="" aria-hidden="true" role="presentation" />
+          </div>
 
-        {/* Botón que abre el submenú de Adopción */}
-        <div className="adopcion-wrap" ref={adopcionWrapRef}>
+          <a href="#">Ubicación</a>
+
+          <div className="vector-divider">
+            <img src={vectorSrc} alt="" aria-hidden="true" role="presentation" />
+          </div>
+
+          {/* Botón que abre el submenú de Adopción */}
+          <div className="adopcion-wrap" ref={adopcionWrapRef}>
+            <Button
+              iconColor="currentColor"
+              iconPosition="left"
+              iconSize="medium"
+              leftIconName="dropLeftIcon"
+              onClick={handleAdopcionClick}
+              rightIconName="dropDownIcon"
+              showLeftIcon={false}
+              showRightIcon
+              size="large"
+              text="Adopción"
+              type="button"
+              variant="btn-text"
+              aria-haspopup="menu"
+              aria-expanded={isAdopcionOpen}
+              aria-controls={adopcionMenuId}
+            />
+
+            <ul
+              id={adopcionMenuId}
+              role="menu"
+              className={`submenu ${isAdopcionOpen ? 'is-open' : ''}`}
+              hidden={!isAdopcionOpen}
+            >
+              <li role="none" className="submenu-li">
+                <a
+                  ref={firstItemRef}
+                  role="menuitem"
+                  href="#"
+                  tabIndex={isAdopcionOpen ? 0 : -1}
+                >
+                  Perritos
+                </a>
+                  <IconSelector
+                  color="var(--blue-normal)"
+                  name="dropRightIcon"
+                  size="medium"
+                  />
+
+              </li>
+              <li role="none" className="submenu-li">
+                <a
+                  role="menuitem"
+                  href="#"
+                  tabIndex={isAdopcionOpen ? 0 : -1}
+                >
+                  Gaticos
+                </a>
+                  <IconSelector
+                  color="var(--blue-normal)"
+                  name="dropRightIcon"
+                  size="medium"
+                  />
+              </li>
+              <li role="none"className="submenu-li">
+                <a
+                  role="menuitem"
+                  href="#"
+                  tabIndex={isAdopcionOpen ? 0 : -1}
+                >
+                  Proceso
+                </a>
+                  <IconSelector
+                  color="var(--blue-normal)"
+                  name="dropRightIcon"
+                  size="medium"
+                  />
+              </li>
+            </ul>
+          </div>
+
           <Button
-            iconColor="currentColor"
-            iconPosition="left"
+            text="Contacto"
+            iconSide="left"
+            showLeftIcon
+            showRightIcon={false}
+            leftIconName="sendIcon"
             iconSize="medium"
-            leftIconName="dropLeftIcon"
-            onClick={handleAdopcionClick}
-            rightIconName="dropDownIcon"
-            showLeftIcon={false}
-            showRightIcon
-            size="large"
-            text="Adopción"
+            iconColor="currentColor"
             type="button"
-            variant="btn-text"
-            aria-haspopup="menu"
-            aria-expanded={isAdopcionOpen}
-            aria-controls={adopcionMenuId}
+            variant="btn-primary"
+            size="large"
           />
-
-          <ul
-            id={adopcionMenuId}
-            role="menu"
-            className={`submenu ${isAdopcionOpen ? 'is-open' : ''}`}
-            hidden={!isAdopcionOpen}
-          >
-            <li role="none" className="submenu-li">
-              <a
-                ref={firstItemRef}
-                role="menuitem"
-                href="#"
-                tabIndex={isAdopcionOpen ? 0 : -1}
-              >
-                Perritos
-              </a>
-                <IconSelector
-                color="var(--blue-normal)"
-                name="dropRightIcon"
-                size="medium"
-                />
-
-            </li>
-            <li role="none" className="submenu-li">
-              <a
-                role="menuitem"
-                href="#"
-                tabIndex={isAdopcionOpen ? 0 : -1}
-              >
-                Gaticos
-              </a>
-                <IconSelector
-                color="var(--blue-normal)"
-                name="dropRightIcon"
-                size="medium"
-                />
-            </li>
-            <li role="none"className="submenu-li">
-              <a
-                role="menuitem"
-                href="#"
-                tabIndex={isAdopcionOpen ? 0 : -1}
-              >
-                Proceso
-              </a>
-                <IconSelector
-                color="var(--blue-normal)"
-                name="dropRightIcon"
-                size="medium"
-                />
-            </li>
-          </ul>
         </div>
-
-        <Button
-          text="Contacto"
-          iconSide="left"
-          showLeftIcon
-          showRightIcon={false}
-          leftIconName="sendIcon"
-          iconSize="medium"
-          iconColor="currentColor"
-          type="button"
-          variant="btn-primary"
-          size="large"
-        />
       </div>
     </div>
   );
