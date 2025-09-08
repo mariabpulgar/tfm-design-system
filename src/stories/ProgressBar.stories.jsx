f9fb00969745381842959ca444d56b0a616d26e3
mariabpulgar <mariabpulgar@gmail.com>
2025-09-08 12:30:28 -0400

    progress bar

--- src/stories/ProgressBar.stories.jsx
+++ src/stories/ProgressBar.stories.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import ProgressBar from "../components/ProgressBar";
+import ProgressBar from "../components/atoms/ProgressBar";
 
 export default {
-  title: 'Components/ProgressBar',
+  title: 'Atoms/ProgressBar',
   component: ProgressBar,
   argTypes: {
     progress: {
