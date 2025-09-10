f1cfc3b7e95dd2c82ad4e7b88675db228f5aae36
mariabpulgar <mariabpulgar@gmail.com>
2025-09-10 11:23:55 -0400

    ruta comodin

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
-import {useState} from 'react';
+
 import Adoptions from './components/templates/Adoptions';
 import Home from './components/templates/Home';
 import AdoptionForm from './components/templates/AdoptionForm';
 import Donation from './components/templates/Donation';
-
 
 function App() {
   const [isAccepted, setIsAccepted] = useState(false);
@@ -15,19 +14,19 @@ function App() {
     setIsAccepted(e.target.checked);
   };
 
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home />} />
         <Route path="/adopciones" element={<Adoptions />} />
-        <Route path="/adoptionForm" element={<AdoptionForm/>}></Route>
-        <Route path="/donation" element={<Donation/>}></Route>
-        {/* aquí más rutas */}
+        <Route path="/adoptionForm" element={<AdoptionForm />} />
+        <Route path="/donation" element={<Donation />} />
+        
+        {/* Ruta comodín: redirige a inicio si no encuentra coincidencia */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
-};
+}
 
-export default App
- 
+export default App;
